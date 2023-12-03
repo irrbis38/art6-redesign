74df13ab1f5677dd5880018d11167ab1561cd22d
Vasil Ilyuhin <v.ilyuhin@gmail.com>
2023-12-03 16:14:56 +0300

    feat: add header

--- sources/src/js/main.js
+++ sources/src/js/main.js
@@ -1,5 +1,28 @@
-document.addEventListener("DOMContentLoaded", function (event) {
-  luxy.init();
-});
+// header logic
+var initHeader = () => {
+  var menu_dropdown_btns = Array.from(
+    document.querySelectorAll(".header_art6__show")
+  );
+  var menu_dropdown_items = menu_dropdown_btns.map((btn) => btn.closest("li"));
 
-// Your functions here
+  if (window.innerWidth > 767) {
+    menu_dropdown_btns.forEach((btn) =>
+      btn.addEventListener("mouseover", () => {
+        var li = btn.closest("li");
+        li.classList.add("active");
+      })
+    );
+
+    menu_dropdown_items.forEach((item) =>
+      item.addEventListener("mouseleave", () => item.classList.remove("active"))
+    );
+  }
+};
+
+// ===== START JS LOGIC AFTER DOM CONTENT LOADED
+document.addEventListener("DOMContentLoaded", function (event) {
+  // init luxy library
+  luxy.init();
+
+  initHeader();
+});
