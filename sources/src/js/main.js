4d2c7e2cfa1dc3ddc10110072963d8bc649ce199
Vasil Ilyuhin <v.ilyuhin@gmail.com>
2023-12-06 10:27:51 +0300

    feat: add toggle mobile menu by burger-button

--- sources/src/js/main.js
+++ sources/src/js/main.js
@@ -1,22 +1,67 @@
 // header logic
 var initHeader = () => {
-  var menu_dropdown_btns = Array.from(
-    document.querySelectorAll(".header_art6__show")
-  );
-  var menu_dropdown_items = menu_dropdown_btns.map((btn) => btn.closest("li"));
+  /*== HEADER MENU LINKS WIDE SCREEN (>991px) ==*/
 
-  if (window.innerWidth > 767) {
+  // get header elements
+  var menu_dropdown_items = Array.from(
+    document.querySelectorAll(".header_art6__item-has-submenu")
+  );
+
+  var menu_dropdown_btns = Array.from(
+    document.querySelectorAll(".header_art6__item-has-submenu button")
+  );
+
+  // descript header handlers
+  var handleMenuBtns = (e) => {
+    var li = e.target.closest("li");
+    li.classList.add("active");
+  };
+
+  var handleDropdownItem = (e) => e.target.classList.remove("active");
+
+  // add / remove header handlers
+  var addHeaderWideScreenListeners = () => {
     menu_dropdown_btns.forEach((btn) =>
-      btn.addEventListener("mouseover", () => {
-        var li = btn.closest("li");
-        li.classList.add("active");
-      })
+      btn.addEventListener("mouseover", handleMenuBtns)
     );
 
     menu_dropdown_items.forEach((item) =>
-      item.addEventListener("mouseleave", () => item.classList.remove("active"))
+      item.addEventListener("mouseleave", handleDropdownItem)
     );
-  }
+  };
+
+  var removeHeaderWideScreenLIsteners = () => {
+    menu_dropdown_btns.forEach((btn) =>
+      btn.removeEventListener("mouseover", handleMenuBtns)
+    );
+
+    menu_dropdown_items.forEach((item) =>
+      item.removeEventListener("mouseleave", handleDropdownItem)
+    );
+  };
+
+  window.innerWidth > 991 && addHeaderWideScreenListeners();
+
+  // toggle header handlers by resize
+  var mq991 = window.matchMedia("(max-width: 991px)");
+
+  var handleMQ = (e) => {
+    e.matches
+      ? removeHeaderWideScreenLIsteners()
+      : addHeaderWideScreenListeners();
+  };
+
+  mq991.addEventListener("change", handleMQ);
+
+  /*== TOGGLE HEADER MOBILE MENU ==*/
+
+  var header = document.getElementById("header");
+  // var mobile_menu = document.getElementById("mobile-menu");
+  var burger_button = document.getElementById("burger-button");
+
+  burger_button.addEventListener("click", () =>
+    header.classList.toggle("mobile-menu-open")
+  );
 };
 
 // ===== START JS LOGIC AFTER DOM CONTENT LOADED
