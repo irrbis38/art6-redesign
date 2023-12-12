4574487fb1471bae69ecce9499c7b6adc421d5a0
Vasil Ilyuhin <v.ilyuhin@gmail.com>
2023-12-12 06:56:55 +0300

    publish unfinish one-file bunle

---
+++ one-file/js/main.js
@@ -0,0 +1,111 @@
+// header logic
+var initHeader = () => {
+  /*== HEADER MENU LINKS WIDE SCREEN (>991px) ==*/
+
+  // get header elements
+  var menu_dropdown_items = Array.from(
+    document.querySelectorAll(".header_art6__item-has-submenu")
+  );
+
+  var menu_dropdown_btns = Array.from(
+    document.querySelectorAll(".header_art6__item-has-submenu button")
+  );
+
+  // descript header handlers
+  var handleMenuBtns = (e) => {
+    var li = e.target.closest("li");
+    li.classList.add("active");
+  };
+
+  var handleDropdownItem = (e) => e.target.classList.remove("active");
+
+  // add / remove header handlers
+  var addHeaderWideScreenListeners = () => {
+    menu_dropdown_btns.forEach((btn) =>
+      btn.addEventListener("mouseover", handleMenuBtns)
+    );
+
+    menu_dropdown_items.forEach((item) =>
+      item.addEventListener("mouseleave", handleDropdownItem)
+    );
+  };
+
+  var removeHeaderWideScreenLIsteners = () => {
+    menu_dropdown_btns.forEach((btn) =>
+      btn.removeEventListener("mouseover", handleMenuBtns)
+    );
+
+    menu_dropdown_items.forEach((item) =>
+      item.removeEventListener("mouseleave", handleDropdownItem)
+    );
+  };
+
+  window.innerWidth > 991 && addHeaderWideScreenListeners();
+
+  // toggle header handlers by resize
+  var mq991 = window.matchMedia("(max-width: 991px)");
+
+  var handleMQ = (e) => {
+    e.matches
+      ? removeHeaderWideScreenLIsteners()
+      : addHeaderWideScreenListeners();
+  };
+
+  mq991.addEventListener("change", handleMQ);
+
+  /*== TOGGLE SUBMENU ON MOBILE SCREEN ==*/
+  var toggle_submenu_buttons = Array.from(
+    document.querySelectorAll(".header_art6__mobile-item-has-submenu button")
+  );
+
+  var handleToggleSubmenuButtons = (e) => {
+    var container = e.target.parentElement;
+    var submenu = e.target.nextElementSibling;
+    var isOpen = container.classList.contains("submenu-opened");
+
+    if (!isOpen) {
+      requestAnimationFrame(() => {
+        container.classList.add("submenu-opened");
+        submenu.style.maxHeight = submenu.scrollHeight + "px";
+      });
+    } else {
+      requestAnimationFrame(() => {
+        container.classList.remove("submenu-opened");
+        submenu.style.maxHeight = 0;
+      });
+    }
+  };
+
+  toggle_submenu_buttons.forEach((btn) =>
+    btn.addEventListener("click", handleToggleSubmenuButtons)
+  );
+
+  /*== TOGGLE HEADER MOBILE MENU ==*/
+  var body = document.body;
+  var header = document.getElementById("header");
+  var burger_button = document.getElementById("burger-button");
+  var submenues = document.querySelectorAll(
+    ".header_art6__mobile-item-has-submenu ul"
+  );
+
+  burger_button.addEventListener("click", () => {
+    header.classList.toggle("mobile-menu-open");
+    body.classList.toggle("lock");
+    submenues.forEach((submenu) => {
+      requestAnimationFrame(() => {
+        var container = submenu.parentElement;
+        container.classList.remove("submenu-opened");
+        submenu.style.maxHeight = 0;
+      });
+    });
+  });
+};
+
+// ===== START JS LOGIC AFTER DOM CONTENT LOADED
+document.addEventListener("DOMContentLoaded", function (event) {
+  // init luxy library
+  luxy.init();
+
+  // init header logic
+  initHeader();
+});
