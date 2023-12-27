79344359ff1dc460526ac0581849bae44224f926
Vasil Ilyuhin <v.ilyuhin@gmail.com>
2023-12-27 14:23:43 +0300

    refactor: change fonts

--- sources/src/js/_libs_concat.cjs
+++ sources/src/js/_libs_concat.cjs
@@ -3,4 +3,6 @@ module.exports = [
   "./src/libs/luxy/luxy.js",
   // "./src/libs/splidejs/splide.min.js",
   "./src/libs/glide/glide.min.js",
+  "./src/libs/gsap/gsap.min.js",
+  "./src/libs/gsap/ScrollTrigger.min.js",
 ];
